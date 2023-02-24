d8d9f30df22c4328a0522274559acaa18d4282ee
thelordzeus <arnav55125@gmail.com>
2023-02-24 10:27:15 +0530

    chore: env setup

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+export default function about() {
+  return (
+    <>
+      <h1>About us page </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eos natus
+        iusto quos asperiores fugiat harum cupiditate quibusdam recusandae
+        animi, cum aliquam dicta dignissimos! Ad quibusdam enim libero non
+        animi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+        nobis harum, sequi ipsum dolores maxime enim quasi odio nesciunt
+        commodi? Illum magnam sequi ipsam incidunt sint recusandae eos
+        asperiores temporibus?
+      </p>
+    </>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,21 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Events Page </h1>
+      <div>
+        <a href="">
+          <img />
+          <h2>Events in Barcelona</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events in Barcelona</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events in Barcelona</h2>
+        </a>
+      </div>
+    </>
+  );
+}
